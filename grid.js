8854a70d82d0152f7f3b277c0acc5a0657ab6aa4
routprakash167-stack <routprakash167@gmail.com>
2026-02-22 23:37:35 +0530

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,1 +1,19 @@
+export function drawGrid(ctx, coord, canvas) {
+  const step = coord.scale;
+  ctx.strokeStyle = "#333";
+  ctx.lineWidth = 1;
 
+  for (let x = coord.offsetX % step; x < canvas.width; x += step) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.stroke();
+  }
+
+  for (let y = coord.offsetY % step; y < canvas.height; y += step) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+  }
+}
