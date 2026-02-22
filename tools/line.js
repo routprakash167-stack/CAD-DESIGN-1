c3600456af292aa61d57ac3a9cc3089345ef532a
routprakash167-stack <routprakash167@gmail.com>
2026-02-22 23:39:56 +0530

    Update line.js

--- tools/line.js
+++ tools/line.js
@@ -1,1 +1,16 @@
+export class Line {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
 
+  draw(ctx, coord, canvas) {
+    const s = coord.worldToScreen(this.start.x, this.start.y, canvas);
+    const e = coord.worldToScreen(this.end.x, this.end.y, canvas);
+    ctx.strokeStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(s.x, s.y);
+    ctx.lineTo(e.x, e.y);
+    ctx.stroke();
+  }
+}
