7b43c13e24693cd57a4a256c7e8bf071302b7c3a
routprakash167-stack <routprakash167@gmail.com>
2026-02-22 23:39:13 +0530

    Update engine.js

--- Core/engine.js
+++ Core/engine.js
@@ -1,1 +1,17 @@
+export class Engine {
+  constructor() {
+    this.objects = [];
+  }
 
+  add(obj) {
+    this.objects.push(obj);
+  }
+
+  remove(obj) {
+    this.objects = this.objects.filter(o => o !== obj);
+  }
+
+  draw(ctx, coord, canvas) {
+    this.objects.forEach(obj => obj.draw(ctx, coord, canvas));
+  }
+}
