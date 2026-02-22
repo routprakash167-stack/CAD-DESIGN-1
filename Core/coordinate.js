197dc320a6351a1f61d7e1e3d0c4ebd0eed68e33
routprakash167-stack <routprakash167@gmail.com>
2026-02-22 23:36:49 +0530

    Update coordinate.js

--- Core/coordinate.js
+++ Core/coordinate.js
@@ -1,1 +1,21 @@
+export class CoordinateSystem {
+  constructor() {
+    this.scale = 50;
+    this.offsetX = 0;
+    this.offsetY = 0;
+  }
 
+  worldToScreen(x, y, canvas) {
+    return {
+      x: canvas.width/2 + (x * this.scale) + this.offsetX,
+      y: canvas.height/2 - (y * this.scale) + this.offsetY
+    };
+  }
+
+  screenToWorld(x, y, canvas) {
+    return {
+      x: (x - canvas.width/2 - this.offsetX) / this.scale,
+      y: -(y - canvas.height/2 - this.offsetY) / this.scale
+    };
+  }
+}
