e2571d81815976d59736e78c14a3e8c5a4987c87
routprakash167-stack <routprakash167@gmail.com>
2026-02-22 23:38:35 +0530

    Update history.js

--- Core/history.js
+++ Core/history.js
@@ -1,1 +1,25 @@
+export class History {
+  constructor() {
+    this.undoStack = [];
+    this.redoStack = [];
+  }
 
+  execute(action) {
+    this.undoStack.push(action);
+    this.redoStack = [];
+  }
+
+  undo(objects) {
+    if (!this.undoStack.length) return;
+    const action = this.undoStack.pop();
+    action.undo(objects);
+    this.redoStack.push(action);
+  }
+
+  redo(objects) {
+    if (!this.redoStack.length) return;
+    const action = this.redoStack.pop();
+    action.redo(objects);
+    this.undoStack.push(action);
+  }
+}
