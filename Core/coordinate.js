86e7f842ddea01a5a7b27969565f48365fb26f7a
routprakash167-stack <routprakash167@gmail.com>
2026-02-22 23:49:03 +0530

    Update coordinate.js

--- Core/coordinate.js
+++ Core/coordinate.js
@@ -7,15 +7,25 @@ export class CoordinateSystem {
 
   worldToScreen(x, y, canvas) {
     return {
-      x: canvas.width/2 + (x * this.scale) + this.offsetX,
-      y: canvas.height/2 - (y * this.scale) + this.offsetY
+      x: canvas.width / 2 + x * this.scale + this.offsetX,
+      y: canvas.height / 2 - y * this.scale + this.offsetY
     };
   }
 
   screenToWorld(x, y, canvas) {
     return {
-      x: (x - canvas.width/2 - this.offsetX) / this.scale,
-      y: -(y - canvas.height/2 - this.offsetY) / this.scale
+      x: (x - canvas.width / 2 - this.offsetX) / this.scale,
+      y: -(y - canvas.height / 2 - this.offsetY) / this.scale
     };
+  } 
+  
+  zoom(factor, mouseX, mouseY, canvas) {
+    const wx = (mouseX - canvas.width / 2 - this.offsetX) / this.scale;
+    const wy = -(mouseY - canvas.height / 2 - this.offsetY) / this.scale;
+
+    this.scale *= factor;
+
+    this.offsetX = mouseX - canvas.width / 2 - wx * this.scale;
+    this.offsetY = mouseY - canvas.height / 2 + wy * this.scale;
   }
 }
