19d1410d3e947ffd44174c7622be1cfc3ffe9d4c
routprakash167-stack <routprakash167@gmail.com>
2026-02-22 23:40:29 +0530

    Update circle.js

--- tools/circle.js
+++ tools/circle.js
@@ -1,1 +1,14 @@
+export class Circle {
+  constructor(center, radius) {
+    this.center = center;
+    this.radius = radius;
+  }
 
+  draw(ctx, coord, canvas) {
+    const c = coord.worldToScreen(this.center.x, this.center.y, canvas);
+    ctx.beginPath();
+    ctx.arc(c.x, c.y, this.radius * coord.scale, 0, Math.PI * 2);
+    ctx.strokeStyle = "yellow";
+    ctx.stroke();
+  }
+}
